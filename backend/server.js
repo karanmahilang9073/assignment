d9666f8474f7b4622d61ec5f9f9523d82c146da4
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-05-01 11:17:27 +0530

    frontend deployment link added

--- backend/server.js
+++ backend/server.js
@@ -3,37 +3,38 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-import {connectDB} from "./config/db.js";
+import { connectDB } from "./config/db.js";
 
 import profileRouter from "./routes/prodileRoutes.js";
 import caseRouter from "./routes/caseRoutes.js";
 import testimonialRouter from "./routes/testimonialRoutes.js";
 
-const app=express();
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-origin:"http://localhost:5173",
-credentials:true
-}));
+    origin: ["http://localhost:5173", "https://assignment-sigma-ashen.vercel.app",],
+    credentials: true,
+  }),
+);
 
 // serve uploaded images
-app.use("/uploads",express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 
 connectDB();
 
-app.get("/",(req,res)=>{
-res.send("completing assignment");
+app.get("/", (req, res) => {
+  res.send("completing assignment");
 });
 
-app.use("/api/profile",profileRouter);
-app.use("/api/case",caseRouter);
-app.use("/api/testimonial",testimonialRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/case", caseRouter);
+app.use("/api/testimonial", testimonialRouter);
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-console.log(`running on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`running on ${PORT}`);
+});
