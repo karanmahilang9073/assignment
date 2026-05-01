903fe3e7640b36a6a8eb1fc051fe8ec040254b37
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-05-01 11:51:42 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-origin:"http://localhost:5173",
-credentials:true
+    origin:"http://localhost:5173",
+    credentials:true
 }));
 
 // serve uploaded images
