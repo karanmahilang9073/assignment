e95c5ed0887bb9e94ecdd08a4e4f94c902f49da5
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-05-01 11:59:16 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://assignment-one-swart.vercel.app",
     credentials:true
 }));
 
